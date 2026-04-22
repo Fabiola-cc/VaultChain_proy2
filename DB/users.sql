5513d161d9014a30a4c8d3374a79d734d984b262
Fabiola Contreras <fcontrerascol@gmail.com>
2026-04-21 19:02:18 -0600

    refactor: reorganizar proyecto para integrar partes

--- DB/users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    public_key TEXT,
-    encrypted_private_key TEXT,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-);
