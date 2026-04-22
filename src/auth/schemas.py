0b10af45804c037f24c1c45805667ea70ba85dd8
Sofiamishel2003 <sofiamishel2003@gmail.com>
2026-04-21 20:59:58 -0600

    Feat: Create Login schema

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -17,3 +17,16 @@ class RegisterResponse(BaseModel):
 class PublicKeyResponse(BaseModel):
     user_id: str
     public_key: str
+
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+ 
+
+class LoginResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+    user_id: str
+    display_name: str
